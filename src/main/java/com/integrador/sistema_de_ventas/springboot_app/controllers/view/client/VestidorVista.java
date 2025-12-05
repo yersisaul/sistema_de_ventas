e770c6de9ed732e2323373d6be9f5dfde093d52b
Cristhina1 <U22327233@utp.edu.pe>
2025-12-05 15:06:34 -0500

    verificando

--- src/main/java/com/integrador/sistema_de_ventas/springboot_app/controllers/view/client/VestidorVista.java
+++ src/main/java/com/integrador/sistema_de_ventas/springboot_app/controllers/view/client/VestidorVista.java
@@ -1,10 +1,13 @@
 package com.integrador.sistema_de_ventas.springboot_app.controllers.view.client;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.integrador.sistema_de_ventas.springboot_app.dto.ProductoDTO;
 import com.integrador.sistema_de_ventas.springboot_app.services.impl.CategoriaServiceImpl;
 import com.integrador.sistema_de_ventas.springboot_app.services.impl.ProductoServiceImpl;
 
@@ -19,7 +22,8 @@ public class VestidorVista {
 
     @GetMapping("/vestidor")
     public String vestidor(Model model){
-        model.addAttribute("listaProductos", productoServiceImpl.obtenerProductosActivos());
+        List<ProductoDTO> listaProductos = productoServiceImpl.obtenerProductosParaCatalogo();
+        model.addAttribute("listaProductos", listaProductos);
         model.addAttribute("listaCategorias", categoriaServiceImpl.obtenerTodasLasCategorias());
         return "client/Vestidor";
     }
